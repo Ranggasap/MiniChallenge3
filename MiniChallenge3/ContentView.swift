030cbfe0d1aff4aae5fd84d7c90fc8680dc752ce
Rangga Saputra <ranggasaputra200412@gmail.com>
2024-07-29 20:48:25 +0700

    Initial Commit

---
+++ MiniChallenge3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniChallenge3
+//
+//  Created by Rangga Saputra on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniChallenge3/MiniChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge3App.swift
+//  MiniChallenge3
+//
+//  Created by Rangga Saputra on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
