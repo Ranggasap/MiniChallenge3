e8879eaa77fffa6bf8f290e9d7cc5ee0b1221469
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-08-18 22:28:15 +0700

    Start & Stop button synchronization between iOS and WatchOS

---
+++ Utils/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  MiniChallenge3
+//
+//  Created by Bryan Vernanda on 18/08/24.
+//
+
+import Foundation
+
+enum stringSent: String {
+    case recording = "recording"
+}
+
+enum messageSent: String {
+    case recordStateChangeRequest = "recordStateChangeRequest"
+    case recordState = "recordState"
+    case done = "done"
+}
