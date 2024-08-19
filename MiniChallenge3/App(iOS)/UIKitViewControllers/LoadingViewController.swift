e4e9b443a0180cb43c1bc52f36963b56bc3be311
Rangga Saputra <ranggasaputra200412@gmail.com>
2024-08-19 21:09:25 +0700

    Animation Done

--- MiniChallenge3/App(iOS)/UIKitViewControllers/LoadingViewController.swift
+++ MiniChallenge3/App(iOS)/UIKitViewControllers/LoadingViewController.swift
@@ -8,21 +8,51 @@
 import UIKit
 
 class LoadingViewController: UIViewController {
+    
+    let deerHorn = UIImageView(image: UIImage(named: "DeerHorn"))
+    let donutBited1 = UIImageView(image: UIImage(named: "DonutBited"))
+    let donutBited2 = UIImageView(image: UIImage(named: "DonutBited"))
+    let donutBited3 = UIImageView(image: UIImage(named: "DonutBited"))
+    let donutBited4 = UIImageView(image: UIImage(named: "DonutBited"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackground()
+        animateDonutsAndDeerHorn()
     }
     
-    
+    func animateDonutsAndDeerHorn() {
+        let donutBitedViews = [donutBited1, donutBited2, donutBited3, donutBited4]
+        
+        // Spring animation for donutBited views
+        for (index, donutView) in donutBitedViews.enumerated() {
+            donutView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            UIView.animate(withDuration: 1.0,
+                           delay: Double(index) * 0.2,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.3,
+                           options: .curveEaseInOut,
+                           animations: {
+                               donutView.transform = .identity
+                           }, completion: nil)
+        }
+        
+        deerHorn.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+            
+        // Animation to show deerHorn with a spring effect after 3 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            UIView.animate(withDuration: 1.0,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.3,
+                           options: .curveEaseInOut,
+                           animations: {
+                               self.deerHorn.transform = .identity
+                           }, completion: nil)
+        }
+    }
 
     func configureBackground(){
-        
-        let deerHorn = UIImageView(image: UIImage(named: "DeerHorn"))
-        let donutBited1 = UIImageView(image: UIImage(named: "DonutBited"))
-        let donutBited2 = UIImageView(image: UIImage(named: "DonutBited"))
-        let donutBited3 = UIImageView(image: UIImage(named: "DonutBited"))
-        let donutBited4 = UIImageView(image: UIImage(named: "DonutBited"))
         
         view.addSubview(deerHorn)
         view.addSubview(donutBited1)
