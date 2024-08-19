d22fd6ec5b10743ce2f5e645d3d5296df78bd5c3
Rangga Saputra <ranggasaputra200412@gmail.com>
2024-08-19 17:56:16 +0700

    Add ViewController Loading

---
+++ MiniChallenge3/App(iOS)/UIKitViewControllers/LoadingViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoadingViewController.swift
+//  MiniChallenge3
+//
+//  Created by Rangga Saputra on 19/08/24.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
