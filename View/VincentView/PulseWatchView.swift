f0fd2ca304b68e8300638472d1371b2e80f4b934
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-08-22 13:59:00 +0700

    implementation watchview, assets, schematic

---
+++ View/VincentView/PulseWatchView.swift
@@ -0,0 +1,18 @@
+//
+//  PulseWatchView.swift
+//  Mini3 Watch App
+//
+//  Created by Vincent Saranang on 22/08/24.
+//
+
+import SwiftUI
+
+struct PulseWatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PulseWatchView()
+}
